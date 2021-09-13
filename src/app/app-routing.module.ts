f8a287470da10ce3cd4c965e6636071a324cfafd
Gabrieldrc <gabriel.drc25@gmail.com>
2021-09-13 14:23:06 -0300

    component layout added and the basics of style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./main/main.module')
-      .then(module => module.MainModule),
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./main/main.module')
+          .then(module => module.MainModule),
+      }
+    ]
   }
 ];
 
